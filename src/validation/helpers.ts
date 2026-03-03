f501671b9db5cb3592b83bef928c8f088b24f33e
Deshenfektion <deshen.r1502@gmail.com>
2026-03-03 19:20:00 +0100

    Add validation rule helpers

---
+++ src/validation/helpers.ts
@@ -0,0 +1,7 @@
+import type { Pokemon } from '../shared/types/pokemon';
+
+export const nonEmptyMoves = (pokemon: Pokemon): readonly string[] =>
+  pokemon.moves.filter((move) => move.trim() !== '');
+
+export const isSkeletonEntry = (pokemon: Pokemon): boolean =>
+  pokemon.ability === undefined && pokemon.item === undefined && nonEmptyMoves(pokemon).length === 0;
