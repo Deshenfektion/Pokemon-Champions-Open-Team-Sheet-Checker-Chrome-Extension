1eced75a6980dbc07fd2320beb890a4245e0ac03
Deshenfektion <deshen.r1502@gmail.com>
2026-03-13 17:25:00 +0100

    Wire content script to respond to team requests

--- src/extension/content/index.ts
+++ src/extension/content/index.ts
@@ -1,1 +1,11 @@
-export {};
+import type { GetTeamResponse } from '../../shared/messages';
+import { isGetTeamRequest } from '../../shared/messages';
+import { extractTeamText } from './extractTeam';
+
+chrome.runtime.onMessage.addListener((message: unknown, _sender, sendResponse) => {
+  if (!isGetTeamRequest(message)) {
+    return;
+  }
+  const response: GetTeamResponse = { teamText: extractTeamText() };
+  sendResponse(response);
+});
